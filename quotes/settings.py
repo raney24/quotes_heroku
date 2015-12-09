4d904f4435efe95062e503803351413791ccbf52
Kyle Raney <raney24fcb@gmail.com>
2015-12-08 19:57:39 -0500

    fix db

--- quotes/settings.py
+++ quotes/settings.py
@@ -111,17 +111,17 @@ WSGI_APPLICATION = 'quotes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#         'NAME': 'quotes_db_test', # Or path to database file if using sqlite3.
-#         # The following settings are not used with sqlite3:
-#         'USER': 'Kyle',
-#         'PASSWORD': 'sfenfcb@$',
-#         'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-#         'PORT': '', 
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'quotes_db_test', # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'Kyle',
+        'PASSWORD': 'sfenfcb@$',
+        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '', 
+    }
+}
 
 
 # Internationalization
