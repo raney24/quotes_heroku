9d86a3535cd63afa8f434ca5ed8245c0a537d1bc
Kyle Raney <raney24fcb@gmail.com>
2015-12-08 19:56:39 -0500

    fix import os

--- quotes/settings.py
+++ quotes/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 from config import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-# import os
+import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -155,7 +155,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-import os
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
