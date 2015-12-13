2d53eda0270c8c92a533d3c74db03d642ba214d7
Kyle Raney <raney24fcb@gmail.com>
2015-12-12 22:29:42 -0500

    whatever

--- stocks/models.py
+++ stocks/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models import Count
 from django.core.urlresolvers import reverse
 import ystockquote
-# from scraper import *
+from scraper import *
 from django.contrib.auth.models import User
 
 class Stock(models.Model):
