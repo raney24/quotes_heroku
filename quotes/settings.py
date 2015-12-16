8156d4c534ba39a1c37bb60269d23b0e14e26f83
Kyle Raney <raney24fcb@gmail.com>
2015-12-16 14:35:51 -0500

    db switch2

--- quotes/settings.py
+++ quotes/settings.py
@@ -111,15 +111,26 @@ WSGI_APPLICATION = 'quotes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#         'NAME': 'quotes_db_test', # Or path to database file if using sqlite3.
+#         # The following settings are not used with sqlite3:
+#         'USER': 'Kyle',
+#         'PASSWORD': 'sfenfcb@$',
+#         'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+#         'PORT': '', 
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'quotes_db_test', # Or path to database file if using sqlite3.
+        'NAME': 'dd5jln4pq9694p', # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'Kyle',
-        'PASSWORD': 'sfenfcb@$',
-        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '', 
+        'USER': 'kefnbskbrnizbh',
+        'PASSWORD': 'kZPqLnyht3IGZEED8pQC5C0eNV',
+        'HOST': 'ec2-54-204-15-48.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '5432', 
     }
 }
 
@@ -148,7 +159,7 @@ STATIC_URL = '/static/'
 import dj_database_url
 
 # COMMENT OUT FOR HEROKU DEPLOYMENT
-DATABASES['default'] =  dj_database_url.config()
+# DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
